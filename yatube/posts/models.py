28860c319c52362bc31a1cd2417ce602a775787a
Margulan Zhanay <mcmrkr@gmail.com>
2023-03-06 16:37:54 +0800

    Make code readable with PEP8 guidelines

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=50)
@@ -10,8 +11,8 @@ class Group(models.Model):
 
     def __str__(self):
         return self.title
-    
-# Create your models here.
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -26,6 +27,3 @@ class Post(models.Model):
         null=True,
         on_delete=models.SET_NULL
     )
-
-
-    
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
-# Create your views here.
+
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'posts' : posts,
+        'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
