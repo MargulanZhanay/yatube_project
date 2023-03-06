ca68d4c185ff8746ec4920df700c949b6cb58a8b
Margulan Zhanay <mcmrkr@gmail.com>
2023-03-06 18:22:37 +0800

    Delete filter in view function instead use related name

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -25,5 +25,6 @@ class Post(models.Model):
         Group,
         blank=True,
         null=True,
-        on_delete=models.SET_NULL
+        on_delete=models.SET_NULL,
+        related_name='posts'
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
+MAGIC_NUMBER = 10  # Number of posts in a page.
+
 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
@@ -13,7 +15,8 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.order_by(
+        '-pub_date')[:MAGIC_NUMBER]
     context = {
         'group': group,
         'posts': posts,
