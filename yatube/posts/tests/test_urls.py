b66d484b6b400476b89d5f3792b90f0a9dc38e65
Margulan Zhanay <mcmrkr@gmail.com>
2023-05-01 02:18:38 +0800

    Add test urls in posts

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.test import TestCase, Client
 from ..models import Post, Group
 from django.contrib.auth import get_user_model
@@ -20,43 +22,53 @@ class PostURLTests(TestCase):
             author=cls.user,
             text='Тестовый пост',
         )
+        cls.templates = [
+            "/",
+            f"/group/{cls.group.slug}/",
+            f"/profile/{cls.user}/",
+            f"/posts/{cls.post.id}/",
+        ]
+        cls.templates_url_names = {
+            "/": "posts/index.html",
+            f"/group/{cls.group.slug}/": "posts/group_list.html",
+            f"/profile/{cls.user.username}/": "posts/profile.html",
+            f"/posts/{cls.post.id}/": "posts/post_detail.html",
+            f"/posts/{cls.post.id}/edit/": "posts/create_post.html",
+            "/create/": "posts/create_post.html",
+        }
 
     def setUp(self):
         self.guest_client = Client()
         self.authorized_client = Client()
         self.authorized_client.force_login(PostURLTests.user)
 
-    def test_home_url_exists_at_desired_location(self):
-        """Page / available to any user."""
-        response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def test_urls_exists_at_desired_location(self):
+        for address in self.templates:
+            with self.subTest(address):
+                response = self.guest_client.get(address)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_group_url_exists_at_desired_location(self):
-        """Page /group/<slug>/ available to any user."""
-        response = self.guest_client.get(f'/group/{self.group.slug}/')
-        self.assertEqual(response.status_code, 200)
+    def test_posts_post_id_edit_url_exists_at_author(self):
+        """Page /posts/post_id/edit/ is available only to the author."""
+        self.user = User.objects.get(username=self.user)
+        self.authorized_client = Client()
+        self.authorized_client.force_login(PostURLTests.user)
+        response = self.authorized_client.get(f"/posts/{self.post.id}/edit/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_post_detail_url_exists_at_desired_location(self):
-        """Page /posts/<int:post_id>/ available to any user."""
-        response = self.guest_client.get(f'/posts/{self.post.pk}/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_profile_url_exists_at_desired_location(self):
-        """Page /profile/ available to any user."""
-        response = self.guest_client.get(f'/profile/{self.user.username}/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_author_post_edit_status_code(self):
-        """Page /posts/<int:post_id>/edit/ available to authorized user."""
-        response = self.authorized_client.get(f'/posts/{self.post.pk}/edit/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_autorized_client_create_post(self):
-        """Page /create/ available to authorized user."""
-        response = self.authorized_client.get('/create/')
-        self.assertEqual(response.status_code, 200)
+    def test_create_url_redirect_anonymous_on_auth_login(self):
+        """The /create/ page is available to an authorized user."""
+        response = self.guest_client.get("/create/", follow=True)
+        self.assertRedirects(response, "/auth/login/?next=/create/")
 
     def test_unexisting_page_at_desired_location(self):
         """Page /unexisting_page/ should give an error."""
         response = self.guest_client.get('/unexisting_page/')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+    def test_urls_uses_correct_template(self):
+        """The URL uses the appropriate template."""
+        for url, template in self.templates_url_names.items():
+            with self.subTest(template=template):
+                response = self.authorized_client.get(url)
+                self.assertTemplateUsed(response, template)
