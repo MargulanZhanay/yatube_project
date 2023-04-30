db6f2fd040528bf9ae19d27d7bb3b60d276b968c
Margulan Zhanay <mcmrkr@gmail.com>
2023-05-01 02:05:47 +0800

    Add tests for help_text and verbose_names in test_models

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -23,6 +23,33 @@ class PostModelTest(TestCase):
         )
 
     def test_models_have_correct_object_names(self):
-        """Проверяем, что у моделей корректно работает __str__."""
+        """Checking that __str__ works correctly for models."""
         post = PostModelTest.post
         self.assertEqual(str(post), post.text[:15])
+
+    def test_verbose_name(self):
+        """Verbose_name in the fields is the same as expected."""
+        field_verboses = {
+            'text': 'Текст',
+            'pub_date': 'pub date',
+            'author': 'author',
+            'group': 'Укажите название вашей группы',
+        }
+        for value, expected in field_verboses.items():
+            with self.subTest(value=value):
+                self.assertEqual(
+                    self.post._meta.get_field(value).verbose_name, expected
+                )
+
+    def test_help_text(self):
+        """Help_text in the fields is the same as expected."""
+        post = PostModelTest.post
+        field_help_texts = {
+            'text': 'Текст вашего поста',
+            'group': '',
+        }
+        for value, expected in field_help_texts.items():
+            with self.subTest(value=value):
+                self.assertEqual(
+                    post._meta.get_field(value).help_text, expected
+                )
